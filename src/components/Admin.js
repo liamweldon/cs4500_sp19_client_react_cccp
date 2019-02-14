16327d71f49286affa3d63549fb9dd74bc0bc1da
Demi Filippou <filippou.d@husky.neu.edu>
2019-02-14 14:32:12 -0500

    first pass at FAQ page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import Services from './Services'
+import React from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import Services from './Services';
+import FAQs from './FAQs';
 
-const Admin = () =>
-<div>
-    <h2>Admin</h2>
-    <Router>
-        <div className="row">
-            <div className="col-3">
-                <br/>
-                <Link to="/admin/services">Services</Link>
+const Admin = () => (
+    <div>
+        <h2>Admin</h2>
+        <Router>
+            <div className="row">
+                <div className="col-3">
+                    <Link to="/admin/services">Services</Link>
+                    <br />
+                    <Link to="/admin/faqs">FAQs</Link>
+                    <br />
+                </div>
+                <div className="col-9">
+                    <Route path="/admin/faqs" exact component={FAQs} />
+                    <Route path="/admin/services" exact component={Services} />
+                </div>
             </div>
-            <div className="col-9">
-                <Route
-                    path="/admin/services"
-                    exact
-                    component={Services}/>
-            </div>
-        </div>
-    </Router>
-</div>
+        </Router>
+    </div>
+);
 
-export default Admin
+export default Admin;
---
+++ src/services/FAQService.js
@@ -0,0 +1,13 @@
+import {API_ROOT} from '../api-config';
+
+export default class FAQService {
+    static instance = null;
+    static getInstance() {
+        if (FAQService.instance === null) {
+            FAQService.instance = new FAQService();
+        }
+        return this.instance;
+    }
+    findFAQById = (id) => fetch(`${API_ROOT}/api/faqs/${id}`).then((response) => response.json());
+    findAllFAQs = () => fetch(`${API_ROOT}/api/faqs`).then((response) => response.json());
+}
