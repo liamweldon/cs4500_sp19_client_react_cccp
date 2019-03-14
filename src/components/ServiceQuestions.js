f48b115f645daaa5a68819d952af4a8ce5a3d899
Kevin Hassenfratz <hassenfratz.k@husky.neu.edu>
2019-03-14 18:22:12 -0400

    Started implementing service question creation

--- src/components/ServiceQuestions.js
+++ src/components/ServiceQuestions.js
@@ -7,9 +7,23 @@ class ServiceQuestions extends React.Component {
         super(props)
         this.serviceQuestionService = ServiceQuestionService.getInstance()
         this.state = {
-            serviceQuestions: []
+            serviceQuestions: [],
+            newServiceQuestion: {
+              serviceType: '',
+              type: '',
+              question: ''
+            }
         }
     }
+
+    getServiceQuestions = () => {
+      this.serviceQuestionService.findAllServiceQuestions().then((questions) =>
+        this.setState({
+          serviceQuestions: questions
+        })
+      );
+    };
+
     componentDidMount() {
         this.serviceQuestionService
             .findAllServiceQuestions()
@@ -19,6 +33,25 @@ class ServiceQuestions extends React.Component {
                 })
             )
     }
+
+    handleNewServiceQuestionInputChange = (e) => {
+      this.setState({newServiceQuestion: {...this.state.newServiceQuestion, [e.target.name]: e.target.value}});
+    };
+
+    addServiceQuestion = () => {
+      const newServiceQuestion = this.state.newServiceQuestion;
+      this.ServiceQuestionService.addServiceQuestion(newServiceQuestion).then((res) => {
+        this.getServiceQuestions();
+        this.setState({
+          newServiceQuestion: {
+          serviceType: '',
+          type: '',
+          question: ''
+        }
+      });
+    });
+  };
+
     render() {
         return(
             <div>
@@ -28,6 +61,27 @@ class ServiceQuestions extends React.Component {
                     <tr className="header-row">
                             <td>Type</td>
                             <td>Question</td>
+                    </tr>
+                    <tr key={-1}>
+                      <td>
+                        <input value={this.state.newServiceQuestion.service} name="service" onChange={this.handleNewServiceQuestionInputChange} />
+                      </td>
+                      <td>
+                        <select
+                          value={this.state.newServiceQuestion.type}
+                          name="type"
+                          onChange={this.handleNewServiceQuestionInputChange}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          value={this.state.newServiceQuestion.question}
+                          name="question"
+                          onChange={this.handleNewServiceQuestionInputChange}
+                        />
+                      </td>
+
+
                     </tr>
                     {this.state.serviceQuestions.map((serviceQuestion) => (
                             <tr key={serviceQuestion.id}>
--- src/services/ServiceQuestionService.js
+++ src/services/ServiceQuestionService.js
@@ -10,5 +10,15 @@ export default class ServiceQuestionService {
     }
     findServiceQuestionById = (id) =>
         fetch(`${API_ROOT}/api/service-questions/${id}`).then((response) => response.json());
+
     findAllServiceQuestions = () => fetch(`${API_ROOT}/api/service-questions`).then((response) => response.json());
+
+    addServiceQuestion = (newServiceQuestion) =>
+    fetch(`${API_ROOT}/api/service-questions/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newServiceQuestion)
+    }).then((response) => response.json());
 }
