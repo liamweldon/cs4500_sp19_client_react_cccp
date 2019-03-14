3cddb0b31c6580a2cef67dcf953d8c49943c01e0
Demi Filippou <filippou.d@husky.neu.edu>
2019-03-13 20:43:56 -0400

    edit toggling

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -3,13 +3,17 @@ import FAQService from '../services/FAQService';
 import './table.scss';
 import {Link} from 'react-router-dom';
 import './FAQs.scss';
+import {omit} from 'lodash';
 
 class FAQs extends React.Component {
   constructor(props) {
     super(props);
     this.faqService = FAQService.getInstance();
     this.state = {
-      faqs: []
+      faqs: [],
+      // key value pairs where the key is the ID and the value is the fields
+      // ex {0: {title: '', question: ''}, 1: {title: '', question: ''}}
+      editing: {}
     };
   }
 
@@ -32,6 +36,16 @@ class FAQs extends React.Component {
 
   editQuestion = () => {};
 
+  toggleEditMode = (question) => {
+    if (question.id in this.state.editing) {
+      this.setState({editing: omit(this.state.editing, question.id)});
+    } else {
+      let editingCopy = {...this.state.editing};
+      editingCopy[question.id] = {title: question.title, question: question.question};
+      this.setState({editing: editingCopy});
+    }
+  };
+
   render() {
     return (
       <div>
@@ -41,13 +55,29 @@ class FAQs extends React.Component {
             <tr className="header-row">
               <td>Title</td>
               <td>Question</td>
+              <td />
+              <td />
+            </tr>
+            <tr key={-1}>
+              <td>
+                <input />
+              </td>
+              <td>
+                <input />
+              </td>
+              <td>
+                <button onClick={() => {}}>
+                  <i className="fas fa-plus-square" />
+                </button>
+              </td>
+              <td />
             </tr>
             {this.state.faqs.map((faq) => (
               <tr key={faq.id}>
                 <td>
-                  <Link to={`/admin/faqs/${faq.id}`}>{faq.title}</Link>
+                  {faq.id in this.state.editing ? <input /> : <Link to={`/admin/faqs/${faq.id}`}>{faq.title}</Link>}
                 </td>
-                <td>{faq.question}</td>
+                <td>{faq.id in this.state.editing ? <input /> : faq.question}</td>
                 <td>
                   <button
                     onClick={() => {
@@ -58,7 +88,7 @@ class FAQs extends React.Component {
                   </button>
                 </td>
                 <td>
-                  <i className="fas fa-pen-square" onClick={this.editQuestion} />
+                  <i className="fas fa-pen-square" onClick={() => this.toggleEditMode(faq)} />
                 </td>
               </tr>
             ))}
