ae9b3b7d4e8d40764b2bcde4de7f29a319d9c2fc
Kevin Hassenfratz <hassenfratz.k@husky.neu.edu>
2019-03-14 20:00:13 -0400

    Implemented service name

--- src/components/ServiceQuestions.js
+++ src/components/ServiceQuestions.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import ServiceQuestionService from '../services/ServiceQuestionService'
+import ServiceQuestionService from '../services/ServiceQuestionService';
+import ServiceService from "../services/ServiceService";
 import './table.scss';
 import {Link} from 'react-router-dom';
 class ServiceQuestions extends React.Component {
     constructor(props) {
         super(props)
         this.serviceQuestionService = ServiceQuestionService.getInstance()
+        this.serviceService = ServiceService.getInstance();
         this.state = {
             serviceQuestions: [],
+            services: [],
             newServiceQuestion: {
-              serviceType: '',
+              serviceName: '',
               type: '',
               question: ''
             }
@@ -31,22 +34,30 @@ class ServiceQuestions extends React.Component {
                 this.setState({
                     serviceQuestions: serviceQuestions
                 })
-            )
+            );
+        this.serviceService
+            .findAllServices()
+            .then(services =>
+                this.setState({
+                    services: services
+                })
+            );
     }
 
     handleNewServiceQuestionInputChange = (e) => {
       this.setState({newServiceQuestion: {...this.state.newServiceQuestion, [e.target.name]: e.target.value}});
     };
 
+
     addServiceQuestion = () => {
       const newServiceQuestion = this.state.newServiceQuestion;
-      this.ServiceQuestionService.addServiceQuestion(newServiceQuestion).then((res) => {
+      this.serviceQuestionService.addServiceQuestion(newServiceQuestion).then((res) => {
         this.getServiceQuestions();
         this.setState({
           newServiceQuestion: {
-          serviceType: '',
-          type: '',
-          question: ''
+            serviceName: '',
+            type: '',
+            question: ''
         }
       });
     });
@@ -59,19 +70,29 @@ class ServiceQuestions extends React.Component {
                 <table className="table">
                     <tbody>
                     <tr className="header-row">
+                            <td>Service</td>
                             <td>Type</td>
                             <td>Question</td>
                     </tr>
                     <tr key={-1}>
                       <td>
-                        <input value={this.state.newServiceQuestion.service} name="service" onChange={this.handleNewServiceQuestionInputChange} />
+                        <select
+                            value={this.state.serviceName}
+                            name="serviceName"
+                            onChange={this.handleNewServiceQuestionInputChange}>
+                            {this.state.services.map(service => (
+                              <option value={service.serviceName}>{service.serviceName}</option>
+                            ))}
+                        </select>
                       </td>
                       <td>
                         <select
-                          value={this.state.newServiceQuestion.type}
                           name="type"
-                          onChange={this.handleNewServiceQuestionInputChange}
-                        />
+                          onChange={this.handleNewServiceQuestionInputChange}>
+                          <option value="Choice">Choice</option>
+                          <option value="True/False">True/False</option>
+                          <option value="Range">Range</option>
+                        </select>
                       </td>
                       <td>
                         <input
@@ -80,11 +101,17 @@ class ServiceQuestions extends React.Component {
                           onChange={this.handleNewServiceQuestionInputChange}
                         />
                       </td>
-
-
+                      <td>
+                        <button onClick={this.addServiceQuestion}>
+                          Create +
+                        </button>
+                      </td>
                     </tr>
                     {this.state.serviceQuestions.map((serviceQuestion) => (
                             <tr key={serviceQuestion.id}>
+                                <td>
+                                    {serviceQuestion.serviceName}
+                                </td>
                                 <td>
                                     {serviceQuestion.type}
                                 </td>
