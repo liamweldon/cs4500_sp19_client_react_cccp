73cd23c60769cdc35bd52466d07404ed479a80e3
Nicolas Westin <nicolaswestin@gmail.com>
2019-03-15 21:19:03 -0400

    Added CRUD operations and basic styling

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -20,53 +20,149 @@ class ServiceDetails extends React.Component {
       const currentServiceId = this.props.match.params.id;
       this.selectService(currentServiceId);
 
-      // this.props.history.push("/admin/services/" + currentService);
       this.setState({
         services: services
       });
     });
   }
 
-  selectService = id =>
-    this.serviceService.findServiceById(id).then(service => {
-      // this.props.history.push("/admin/services/" + id);
+  createService = () => {
+    this.serviceService
+      .createService(this.state.service)
+      .then(this.findAllServices);
+  };
+
+  deleteService = id => {
+    this.serviceService.deleteService(id).then(this.findAllServices);
+  };
+
+  updateService = () => {
+    this.serviceService
+      .updateService(this.state.service)
+      .then(this.findAllServices);
+  };
+
+  selectService = id => {
+    this.serviceService.findServiceById(id).then(service =>
       this.setState({
         service: service
-      });
+      })
+    );
+  };
+
+  updateNameInForm = e => {
+    this.setState({
+      service: {
+        id: this.state.service.id,
+        serviceName: e.target.value,
+        serviceDescription: this.state.service.serviceDescription
+      }
     });
+  };
+
+  updateDescriptionInForm = e => {
+    this.setState({
+      service: {
+        id: this.state.service.id,
+        serviceName: this.state.service.serviceName,
+        serviceDescription: e.target.value
+      }
+    });
+  };
 
   render() {
     return (
-      <div>
-        <h3>Service Details</h3>
-        <select
-          value={this.state.service.id}
-          onChange={e => this.selectService(e.target.value)}
-          className="form-control"
-        >
-          {this.state.services.map(service => {
-            var selected = service.id === this.state.service.id ? true : false;
-            return (
-              <option
-                value={service.id}
-                key={service.id}
-                defaultValue={selected}
-              >
-                {service.serviceName}
-              </option>
-            );
-          })}
-        </select>
-        <label>Service Title</label>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h3>Service Details</h3>
+            <select
+              value={this.state.service.id}
+              onChange={e => {
+                this.selectService(e.target.value);
+                this.props.history.push("/admin/services/" + e.target.value);
+              }}
+              className="form-control"
+            >
+              {this.state.services.map(service => {
+                var selected =
+                  service.id === this.state.service.id ? true : false;
+                return (
+                  <option
+                    value={service.id}
+                    key={service.id}
+                    defaultValue={selected}
+                  >
+                    {service.serviceName}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
         <br />
-        <input
-          className="form-control"
-          type="text"
-          value={this.state.service.serviceName}
-          onChange={() => {
-            console.log("input changed");
-          }}
-        />
+        <div class="row">
+          <div class="col">
+            <label>Service Title</label>
+            <input
+              type="text"
+              class="form-control"
+              //value={this.state.service.serviceName}
+            />
+          </div>
+        </div>
+        <br />
+        <div class="row">
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-danger"
+              onClick={() => this.props.history.push("/admin/services")}
+            >
+              Cancel
+            </button>
+          </div>
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-danger"
+              onClick={() =>
+                this.deleteService().then(() =>
+                  this.props.history.push("/admin/services")
+                )
+              }
+            >
+              Delete
+            </button>
+          </div>
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-primary"
+              onClick={() =>
+                this.updateService().then(() =>
+                  this.props.history.push("/admin/services")
+                )
+              }
+            >
+              Edit
+            </button>
+          </div>
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-success"
+              onClick={() =>
+                this.createService().then(
+                  () => this.props.history.push("/admin/services")
+                  //<Redirect to='/dashboard' />
+                )
+              }
+            >
+              Create
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
