941aeb119276243cd5a8d62ed49187fac8c909df
Abigail Emrath <emrath.a@husky.neu.edu>
2019-03-21 19:04:11 -0400

    Update ServiceAnswers.js

--- src/components/ServiceAnswers.js
+++ src/components/ServiceAnswers.js
@@ -9,90 +9,41 @@ class ServiceAnswers extends React.Component {
         super(props)
         this.serviceAnswerService = ServiceAnswerService.getInstance()
         this.state = {
-            serviceAnswers: [],
-            newAnswer: ""
+            serviceAnswers: []
         }
     }
     componentDidMount() {
-        this.getServiceAnswers()
+        this.serviceAnswerService
+            .findAllServiceAnswers()
+            .then(serviceAnswers =>
+                this.setState({
+                    serviceAnswers: serviceAnswers
+                })
+            )
     }
-
-  getServiceAnswers() {
-    this.serviceAnswerService.findAllServiceAnswers().then(serviceAnswers => {
-      if (serviceAnswers.length) {
-        this.setState({
-          serviceAnswers: serviceAnswers
-        });
-      }
-    });
-  }
-
-  addServiceAnswer = () => {
-    const newAnswer  = this.state.newAnswer;
-    this.serviceAnswerService
-      .addServiceAnswer({ answer: newAnswer })
-      .then(res => {
-        this.getServiceAnswers();
-        this.setState({ newAnswer: "" });
-      });
-  };
-
-
-  deleteServiceAnswer = (aId) => {
-    this.serviceAnswerService
-      .deleteServiceAnswer(aId)
-      .then(res => this.getServiceAnswers());
-  };
-
-
-  render() {
-    return (
-      <div>
-        <h3>Service Answers</h3>
-        <table className="table">
-          <tbody>
-            <tr className="header-row">
-              <td>Question</td>
-              <td>Answer</td>
-              <td />
-            </tr>
-            <tr key={-1}>
-              <td />
-              <td />
-              <td>
-                <input value={this.state.newAnswer} name="answer" onChange={this.handleAnswerInput} />
-              </td>
-              <td>
-                <button onClick={this.addAnswer}>
-                  <i className="fas fa-plus-square" />
-                </button>
-              </td>
-              <td />
-            </tr>
-            {this.state.serviceAnswers.map((serviceAnswer) => (
-                <tr key={serviceAnswer.id}>
-                  <td>{serviceAnswer}</td>
-                  <td>
-                    <Link to={`/admin/service-answers/${serviceAnswer.id}`}>
-                      {serviceAnswer.choiceAnswer}
-                    </Link>
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        this.deleteAnswer(serviceAnswer.id);
-                      }}
-                    >
-                      <i className="fas fa-trash-alt" />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return(
+            <div>
+                <h3>Service Answers</h3>
+                <table className="table">
+                    <tbody>
+                    <tr className="header-row">
+                            <td>Question</td>
+                            <td>Answer</td>
+                    </tr>
+                    {this.state.serviceAnswers.map((serviceAnswer) => (
+                            <tr key={serviceAnswer.id}>
+                                <td>
+                                    {serviceAnswer.serviceQuestion}
+                                </td>
+                                <td><Link to={`/admin/service-answers/${serviceAnswer.id}`}>{serviceAnswer.id}</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default ServiceAnswers;
