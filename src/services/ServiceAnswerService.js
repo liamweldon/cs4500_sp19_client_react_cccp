152b3f4bbd9bcc7aed4c25f7e4f6e7e96f3cb024
ABIGAIL EMRATH <aemrath@cloudhealthtech.com>
2019-03-21 18:02:57 +0000

    added some crud features for service answers

--- src/services/ServiceAnswerService.js
+++ src/services/ServiceAnswerService.js
@@ -10,4 +10,27 @@ export default class ServiceAnswerService {
     }
     findServiceAnswerById = (id) => fetch(`${API_ROOT}/api/service-answers/${id}`).then((response) => response.json());
     findAllServiceAnswers = () => fetch(`${API_ROOT}/api/service-answers`).then((response) => response.json());
+
+      deleteServiceAnswer = (qId, aId) =>
+        fetch(`${API_ROOT}/api/service-questions/${qId}/sas/${aId}`, {
+          method: "DELETE"
+        }).then(response => response.json()).catch(err => err);
+
+      addServiceAnswer = (qId, newAnswer) =>
+        fetch(`${API_ROOT}/api/service-questions/${qId}/sas`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(newAnswer)
+        }).then(response => response.json());
+
+      editServiceAnswer = (id, newAnswer) =>
+        fetch(`${API_ROOT}/api/service-answers/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(newAnswer)
+        }).then(response => response.json());
 }
