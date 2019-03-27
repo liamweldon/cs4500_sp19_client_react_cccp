8bef865dfeb450ac4e7ca8e35c7dc9b52ea64e59
Nicolas Westin <nicolaswestin@gmail.com>
2019-03-26 20:24:20 -0400

    Formatting

--- src/components/ServicesContainer.js
+++ src/components/ServicesContainer.js
@@ -8,14 +8,18 @@ class ServicesContainer extends React.Component {
     this.serviceService = this.props.service;
     this.state = {
       services: [],
-      service: {id: 0, serviceName: "Service Name", serviceDescription: "Description"}
+      service: {
+        id: 0,
+        serviceName: "Service Name",
+        serviceDescription: "Description"
+      }
     };
-  };
+  }
 
-  componentDidMount() { 
+  componentDidMount() {
     console.log("Mounting");
-    return this.findAllServices() 
-  };
+    return this.findAllServices();
+  }
 
   findAllServices = () => {
     this.serviceService.findAllServices().then(services =>
@@ -26,55 +30,65 @@ class ServicesContainer extends React.Component {
   };
 
   createService = () => {
-    this.serviceService.createService(this.state.service).then(this.findAllServices)
-  }; 
+    this.serviceService
+      .createService(this.state.service)
+      .then(this.findAllServices);
+  };
 
   deleteService = id => {
-    this.serviceService.deleteService(id).then(this.findAllServices)
+    this.serviceService.deleteService(id).then(this.findAllServices);
   };
 
   selectService = id => {
-    this.serviceService.findServiceById(id).then(service => this.setState({
-      service: service
-    }))
+    this.serviceService.findServiceById(id).then(service =>
+      this.setState({
+        service: service
+      })
+    );
   };
 
   updateService = () => {
-    this.serviceService.updateService(this.state.service).then(this.findAllServices)
+    this.serviceService
+      .updateService(this.state.service)
+      .then(this.findAllServices);
   };
 
   updateNameInForm = e => {
     this.setState({
-       service: {
-         id: this.state.service.id,
-         serviceName: e.target.value,
-         serviceDescription: this.state.service.serviceDescription
-       }
-    })
+      service: {
+        id: this.state.service.id,
+        serviceName: e.target.value,
+        serviceDescription: this.state.service.serviceDescription
+      }
+    });
   };
 
   updateDescriptionInForm = e => {
     this.setState({
-       service: {
-         id: this.state.service.id,
-         serviceName: this.state.service.serviceName,
-         serviceDescription: e.target.value
-       }
-    })
+      service: {
+        id: this.state.service.id,
+        serviceName: this.state.service.serviceName,
+        serviceDescription: e.target.value
+      }
+    });
   };
 
   render() {
     console.log("Passing services");
     console.log(this.state.services);
-    return (<Services services = {this.state.services}
-                      service = {this.state.service}
-                      findAllServices = {this.findAllServices}
-                      createService = {this.createService}
-                      deleteService = {this.deleteService}
-                      selectService = {this.selectService}
-                      updateService = {this.updateService}
-                      updateNameInForm = {this.updateNameInForm}
-                      updateDescriptionInForm = {this.updateDescriptionInForm}/>);
+    return (
+      <Services
+        services={this.state.services}
+        service={this.state.service}
+        findAllServices={this.findAllServices}
+        createService={this.createService}
+        deleteService={this.deleteService}
+        selectService={this.selectService}
+        updateService={this.updateService}
+        updateNameInForm={this.updateNameInForm}
+        updateDescriptionInForm={this.updateDescriptionInForm}
+      />
+    );
   }
 }
 
