15a036606b6c7b95075e8675576e547fa783cd7c
Nicolas Westin <nicolaswestin@gmail.com>
2019-03-26 23:10:10 -0400

    Formatting

--- src/test/services/ServiceService.test.js
+++ src/test/services/ServiceService.test.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import services from '../mockData/services.mock.json'
-import ServiceService from '../../services/ServiceService'
-import mockService from "./mocks/MockServiceService"
+import services from "../mockData/services.mock.json";
+import ServiceService from "../../services/ServiceService";
+import mockService from "./mocks/MockServiceService";
 
 const test_new_service = {
-    "id": 555,
-    "serviceName": "Car Wash",
-    "serviceDescription": "Clean your car!",
-    "providers": [],
-    "serviceCategories": [],
-    "serviceQuestions": []
- };
+  id: 555,
+  serviceName: "Car Wash",
+  serviceDescription: "Clean your car!",
+  providers: [],
+  serviceCategories: [],
+  serviceQuestions: []
+};
 
 // Service mocking code
 const serviceService = ServiceService.getInstance();
@@ -21,7 +20,7 @@ beforeAll(() => {
 });
 
 // Test service mocking code behaves properly
-test('Test that finding all services works properly', () => {
+test("Test that finding all services works properly", () => {
   return serviceService.findAllServices().then(services => {
     expect(services).toBeDefined();
     expect(services).toHaveLength(3);
@@ -31,9 +30,8 @@ test('Test that finding all services works properly', () => {
   });
 });
 
-test('Test that creation works properly', () => {
+test("Test that creation works properly", () => {
   return serviceService.createService(test_new_service).then(service => {
-
     expect(service).toBeDefined();
     expect(service.id).toBe(555);
 
@@ -46,12 +44,8 @@ test('Test that creation works properly', () => {
   });
 });
 
-test('Test that delete works properly', () => {
+test("Test that delete works properly", () => {
   serviceService.deleteService(123).then(response => {
-
-    expect(service).toBeDefined();
-    expect(service.id).toBe(555);
-
     expect(services).toBeDefined();
     expect(services).toHaveLength(3);
     expect(services[0].id).toBe(234);
