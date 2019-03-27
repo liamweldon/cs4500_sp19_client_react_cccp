981b1183b632d9be531a00d99792a14cc0cc8f59
Kevin Hassenfratz <hassenfratz.k@husky.neu.edu>
2019-03-26 21:52:33 -0400

    Attempting to reslove merge conflicts

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -5,8 +5,8 @@ import FAQDetailsContainer from './FAQDetailsContainer';
 import ServicesContainer from './ServicesContainer';
 import ServiceService from "../services/ServiceService";
 import ServiceDetails from "./ServiceDetails";
-import UserDetailsContainer from './UserDetailsContainer';
-import UsersContainer from './UsersContainer';
+import Users from './Users';
+import UserDetails from './UserDetails';
 import FAQAnswers from "./FAQAnswers";
 import ServiceQuestions from './ServiceQuestions';
 import ServiceQuestionsContainer from './ServiceQuestionsContainer';
@@ -22,32 +22,31 @@ const Admin = () => (
             <div className="row">
                 <div className="col-3">
                     <Link to="/admin/services">Services</Link>
-                    <br/>
+                    <br />
                     <Link to="/admin/faqs">FAQs</Link>
-                    <br/>
+                    <br />
                     <Link to="/admin/faas">FAQ Answers</Link>
-                    <br/>
+                    <br />
                     <Link to="/admin/users">Users</Link>
-                    <br/>
+                    <br />
                     <Link to="/admin/service-questions">Service Questions</Link>
-                    <br/>
+                    <br />
                     <Link to="/admin/service-answers">Service Answers</Link>
-                    <br/>
+                    <br />
                 </div>
                 <div className="col-9">
-                    <Route path="/admin/faqs" exact component={FAQsContainer}/>
-                    <Route path="/admin/faas" exact component={FAQAnswers}/>
-                    <Route path="/admin/faas/:id" exact component={FAQAnswerDetails}/>
-                    <Route path="/admin/services" exact
-                           render={() => <ServicesContainer service={ServiceService.getInstance()}/>}/>
-                    <Route path="/admin/services/:id" exact component={ServiceDetails}/>
-                    <Route path="/admin/users" exact component={UsersContainer}/>
-                    <Route path="/admin/users/:id" exact component={UserDetailsContainer}/>
-                    <Route path="/admin/faqs/:id" exact component={FAQDetailsContainer}/>
-                    <Route path="/admin/service-questions" exact component={ServiceQuestionsContainer}/>
-                    <Route path="/admin/service-questions/:id" exact component={ServiceQuestionDetails}/>
-                    <Route path="/admin/service-answers" exact component={ServiceAnswers}/>
-                    <Route path="/admin/service-answers/:id" exact component={ServiceAnswerDetails}/>
+                    <Route path="/admin/faqs" exact component={FAQsContainer} />
+                    <Route path="/admin/faas" exact component={FAQAnswers} />
+                    <Route path="/admin/faas/:id" exact component={FAQAnswerDetails} />
+                    <Route path="/admin/services" exact render={() => <ServicesContainer service={ServiceService.getInstance()}/>} />
+                    <Route path="/admin/services/:id" exact component={ServiceDetails} />
+                    <Route path="/admin/users" exact component={Users} />
+                    <Route path="/admin/users/:id" exact component={UserDetails}/>
+                    <Route path="/admin/faqs/:id" exact component={FAQDetailsContainer} />
+                    <Route path="/admin/service-questions" exact component={ServiceQuestionsContainer} />
+                    <Route path="/admin/service-questions/:id" exact component={ServiceQuestionDetails} />
+                    <Route path="/admin/service-answers" exact component={ServiceAnswers} />
+                    <Route path="/admin/service-answers/:id" exact component={ServiceAnswerDetails} />
                 </div>
             </div>
         </Router>
