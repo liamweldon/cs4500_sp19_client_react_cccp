89902e2723b9beae94782afe9c90618c598aa016
Nicolas Westin <nicolaswestin@gmail.com>
2019-03-28 23:07:33 -0400

    Added methods for Mocking other ServiceService functions

--- src/test/services/mocks/MockServiceService.js
+++ src/test/services/mocks/MockServiceService.js
@@ -1,9 +1,26 @@
 import { API_ROOT } from "../../../api-config";
 
-const mockService = s =>
+const mockServiceService = s =>
   jest.fn().mockImplementation((url, config) => {
     if (!config) {
-      if (url.indexOf(`${API_ROOT}/api/services`) != -1) {
+      // Find By Id
+      if (
+        url.indexOf(`${API_ROOT}/api/services/`) !== -1 &&
+        url.length > `${API_ROOT}/api/services/`.length
+      ) {
+        const idIndex = url.indexOf("/api/services/") + 14;
+        const serviceId = url.substring(idIndex);
+        return new Promise((resolve, reject) => {
+          resolve({
+            json: function() {
+              return s.find(e => e.id === serviceId);
+            }
+          });
+        });
+      }
+
+      // Find All
+      if (url.indexOf(`${API_ROOT}/api/services`) !== -1) {
         return new Promise((resolve, reject) => {
           resolve({
             json: function() {
@@ -13,18 +30,47 @@ const mockService = s =>
         });
       }
     } else if (config.method === "post") {
-      let service = JSON.parse(config.body);
-      service.id = 555;
-      s.push(service);
-      return new Promise((resolve, reject) => {
-        resolve({
-          json: function() {
-            return service;
-          }
+      const service = JSON.parse(config.body);
+      // Create
+      if (url.indexOf(`${API_ROOT}/api/services/`) !== -1) {
+        const service = JSON.parse(config.body);
+        service.id = 555;
+        s.push(service);
+        return new Promise((resolve, reject) => {
+          resolve({
+            json: function() {
+              return service;
+            }
+          });
         });
-      });
-    } else if (config.method === "delete") {
-      if (url.indexOf(`${API_ROOT}/api/services/123`) != -1) {
+      }
+      // Update
+      else if (url.indexOf(`${API_ROOT}/api/services/${service.id}`) !== -1) {
+        const old_service = s.find(e => e.id === service.id);
+        if (old_service !== undefined) {
+          const updated_service = JSON.parse(JSON.stringify(old_service));
+          s.splice(s.indexOf(old_service), 1, updated_service);
+          return new Promise((resolve, reject) => {
+            resolve({
+              json: function() {
+                return updated_service;
+              }
+            });
+          });
+        } else {
+          return new Promise((resolve, reject) => {
+            reject({
+              json: function() {
+                return undefined;
+              }
+            });
+          });
+        }
+      }
+    }
+    // Delete
+    else if (config.method === "delete") {
+      if (url.indexOf(`${API_ROOT}/api/services/123`) !== -1) {
         delete s[0];
         return new Promise((resolve, reject) => {
           resolve({ json: function() {} });
@@ -33,4 +79,4 @@ const mockService = s =>
     }
   });
 
-export default mockService;
+export default mockServiceService;
