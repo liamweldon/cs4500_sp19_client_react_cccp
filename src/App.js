ef541d469aca12b498d372e1a81d816aea3c86e0
Demi Filippou <filippou.d@husky.neu.edu>
2019-04-01 15:08:30 -0400

    provider page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import Admin from './components/Admin';
+import ProviderContainer from './containers/ProviderContainer';
 
 class App extends Component {
   render() {
@@ -11,8 +12,9 @@ class App extends Component {
         <h1>ServicesRus</h1>
         <Router>
           <div>
-            <Link to="/admin">Admin</Link>
+            <Link to="/admin">Admin</Link> | <Link to="/provider">Provider</Link>
             <Route path="/admin" component={Admin} />
+            <Route path="/provider" exact component={ProviderContainer} />
           </div>
         </Router>
       </div>
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -14,42 +14,45 @@ import ServiceAnswerDetails from './ServiceAnswerDetails';
 import FAQAnswerDetails from './FAQAnswerDetails';
 import FAQAnswers from './FAQAnswers';
 const Admin = () => (
-    <div>
-        <h2>Admin</h2>
-        <Router>
-            <div className="row">
-                <div className="col-3">
-                    <Link to="/admin/services">Services</Link>
-                    <br/>
-                    <Link to="/admin/faqs">FAQs</Link>
-                    <br/>
-                    <Link to="/admin/faas">FAQ Answers</Link>
-                    <br/>
-                    <Link to="/admin/users">Users</Link>
-                    <br/>
-                    <Link to="/admin/service-questions">Service Questions</Link>
-                    <br/>
-                    <Link to="/admin/service-answers">Service Answers</Link>
-                    <br/>
-                </div>
-                <div className="col-9">
-                    <Route path="/admin/faqs" exact component={FAQsContainer}/>
-                    <Route path="/admin/faas" exact component={FAQAnswers}/>
-                    <Route path="/admin/faas/:id" exact component={FAQAnswerDetails}/>
-                    <Route path="/admin/services" exact
-                           render={() => <ServicesContainer service={ServiceService.getInstance()}/>}/>
-                    <Route path="/admin/services/:id" exact component={ServiceDetails}/>
-                    <Route path="/admin/users" exact component={UsersContainer}/>
-                    <Route path="/admin/users/:id" exact component={UserDetailsContainer}/>
-                    <Route path="/admin/faqs/:id" exact component={FAQDetailsContainer}/>
-                    <Route path="/admin/service-questions" exact component={ServiceQuestionsContainer}/>
-                    <Route path="/admin/service-questions/:id" exact component={ServiceQuestionDetails}/>
-                    <Route path="/admin/service-answers" exact component={ServiceAnswersContainer}/>
-                    <Route path="/admin/service-answers/:id" exact component={ServiceAnswerDetails}/>
-                </div>
-            </div>
-        </Router>
-    </div>
+  <div>
+    <h2>Admin</h2>
+    <Router>
+      <div className="row">
+        <div className="col-3">
+          <Link to="/admin/services">Services</Link>
+          <br />
+          <Link to="/admin/faqs">FAQs</Link>
+          <br />
+          <Link to="/admin/faas">FAQ Answers</Link>
+          <br />
+          <Link to="/admin/users">Users</Link>
+          <br />
+          <Link to="/admin/service-questions">Service Questions</Link>
+          <br />
+          <Link to="/admin/service-answers">Service Answers</Link>
+          <br />
+        </div>
+        <div className="col-9">
+          <Route path="/admin/faqs" exact component={FAQsContainer} />
+          <Route path="/admin/faas" exact component={FAQAnswers} />
+          <Route path="/admin/faas/:id" exact component={FAQAnswerDetails} />
+          <Route
+            path="/admin/services"
+            exact
+            render={() => <ServicesContainer service={ServiceService.getInstance()} />}
+          />
+          <Route path="/admin/services/:id" exact component={ServiceDetails} />
+          <Route path="/admin/users" exact component={UsersContainer} />
+          <Route path="/admin/users/:id" exact component={UserDetailsContainer} />
+          <Route path="/admin/faqs/:id" exact component={FAQDetailsContainer} />
+          <Route path="/admin/service-questions" exact component={ServiceQuestionsContainer} />
+          <Route path="/admin/service-questions/:id" exact component={ServiceQuestionDetails} />
+          <Route path="/admin/service-answers" exact component={ServiceAnswersContainer} />
+          <Route path="/admin/service-answers/:id" exact component={ServiceAnswerDetails} />
+        </div>
+      </div>
+    </Router>
+  </div>
 );
 
 export default Admin;
