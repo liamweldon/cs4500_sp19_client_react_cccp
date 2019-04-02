1088ce02716007317266f923c54eb0b5c4776907
Carter Tune <cartertune@gmail.com>
2019-04-02 15:57:25 -0400

    more work

--- src/components/Provider/Reviews.js
+++ src/components/Provider/Reviews.js
@@ -1,6 +1,8 @@
 import React from "react";
 import _ from "lodash";
 import faker from "faker";
+import moment from "moment";
+import "./Provider.scss";
 
 const reviewSortOptions = [
   "Recommended",
@@ -19,7 +21,7 @@ class Reviews extends React.Component {
     this.state = {
       reviews: [],
       sortedBy: reviewSortOptions[0],
-      currPage: 1,
+      currPage: 1
     };
   }
 
@@ -39,10 +41,10 @@ class Reviews extends React.Component {
       // response: STring
       review.id = faker.random.number();
       review.userName = faker.name.findName();
-      review.rating = faker.random.number({ max: 5, min: 1 });
+      review.score = faker.random.number({ max: 5, min: 1 });
       review.date = faker.date.recent(20);
-      review.text = faker.lorem.sentences();
-      review.response = faker.lorem.sentence();
+      review.reviewText = faker.lorem.sentences();
+      review.providerReply = faker.lorem.sentence();
       mockReviews.push(review);
       this.setState({ reviews: mockReviews });
     };
@@ -51,34 +53,84 @@ class Reviews extends React.Component {
   }
 
   getSortedReviews() {
-   return null;
+    return this.state.reviews;
   }
 
   renderHeader() {
     return (
-      <div className="review-header">
-        <h4> Reviews </h4>
+      <div>
+        <div className="row">
+          <div className="col-8">
+            <a name="reviews" />
+            <h3>Reviews</h3>
+          </div>
+          <hr />
+          <div className="col-4">
+            <select className="form-control float-right">
+              <option>Recommended</option>
+              <option>Highest rated</option>
+              <option>Lowest rated</option>
+              <option>Newest first</option>
+              <option>Oldest first</option>
+            </select>
+            <br />
+            <br />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <h4>4.4</h4>
+            <i className="fa fa-star cs4500-yellow wd-font-size-2-em" />
+            <i className="fa fa-star cs4500-yellow wd-font-size-2-em" />
+            <i className="fa fa-star cs4500-yellow wd-font-size-2-em" />
+            <i className="fa fa-star cs4500-yellow wd-font-size-2-em" />
+            <i className="fa fa-star cs4500-yellow wd-font-size-2-em" />
+            <br />
+            {this.state.reviews.length} reviews
+          </div>
+          {/*<div className="col-8">*/}
+            {/*{provider.ratingScores.map((score, index) => (*/}
+              {/*<Rating key={index} index={5 - index} score={score} />*/}
+            {/*))}*/}
+          {/*</div>*/}
+        </div>
+        <br />
       </div>
     );
   }
 
-  renderReview(review) {
-   return null;
+  renderStars(n) {
+    return _.times(n, i => {
+      return <i className={"fa fa-star"} key={i} />;
+    });
   }
 
   renderReviews() {
     return (
       <div>
-        {_.forEach(this.getSortedReviews(), r => {
-          this.renderReview(r);
+        {_.map(this.getSortedReviews().slice(0, 2), review => {
+          return (
+            <div key={review.id}>
+              <div>
+                <h6>{review.userName}</h6>
+              </div>
+              <br />
+              <div>{this.renderStars(review.score)}</div>
+              <br />
+              <div>{review.reviewText}</div>
+              <br />
+              <div>{moment(review.data).format("MMM D, YYYY")}</div>
+              <br />
+              <div>{review.providerReply}</div>
+              <hr />
+            </div>
+          );
         })}
       </div>
     );
   }
 
-  renderPaginator() {
-
-  }
+  renderPaginator() {}
 
   render() {
     return (
