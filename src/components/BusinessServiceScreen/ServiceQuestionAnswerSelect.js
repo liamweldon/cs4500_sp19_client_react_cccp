a119de244c128428599acc128caae8717dde59ab
Nicolas Westin <nicolaswestin@gmail.com>
2019-04-10 00:29:07 -0400

    Filler html until implementation is complete

--- src/components/BusinessServiceScreen/ServiceQuestionAnswerSelect.js
+++ src/components/BusinessServiceScreen/ServiceQuestionAnswerSelect.js
@@ -1,12 +1,38 @@
 import React from "react";
 
 const ServiceQuestionAnswerSelect = props => {
-  const services = props.services;
-  const selectedServiceIds = props.selectedServiceIds;
+  const clickedService = props.clickedSelectedService;
+
+  const serviceItemStyle = {
+    display: "flex",
+    justifyContent: "space-between"
+  };
+
+  const questionsAnswers = () =>
+    clickedService ? (
+      <div>
+        <div>{clickedService.serviceName}</div>
+        <div>
+          <i class="fa fa-times" />
+        </div>
+      </div>
+    ) : (
+      <div>
+        <div>No service...</div>
+        <div>
+          <i class="fa fa-times" />
+        </div>
+      </div>
+    );
 
   return (
     <div>
       <h4>Service Question Answer Select</h4>
+      <ul className="list-group">
+        <li className="list-group-item" style={serviceItemStyle}>
+          {questionsAnswers()}
+        </li>
+      </ul>
     </div>
   );
 };
